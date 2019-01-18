7ca4beb23bd75d5e60a440d655378b8f212c21d5
weston <boldt.weston@gmail.com>
2019-01-17 21:37:29 -0600

    don't need to manually add filename into require entities anymore

--- require-entities.lua
+++ require-entities.lua
@@ -1,11 +1,22 @@
+function recursiveRequire(folder,tree)
+    local tree = tree or {}
+    for i,file in ipairs(love.filesystem.getDirectoryItems(folder)) do
+        local filename = folder .. "/" .. file
+        local dirInfo = love.filesystem.getInfo(filename)
+        if dirInfo.type == "directory" then
+            recursiveRequire(filename)
+        elseif file:match(".lua") == ".lua"
+        and not file:match(".swp") then
+            filename = filename:gsub(".lua","")
+            require(filename)
+        end
+    end
+
+    return tree
+end
+
 return  {
-    require('entities.player');
-
-    require('triggers.mapEnd');
-    require('triggers.enemyCollision');
-
-    require('entities.baseEnemy');
-
-    require('states.running');
-    require('states.player_running');
+    recursiveRequire('entities');
+    recursiveRequire('triggers');
+    recursiveRequire('states');
 }
